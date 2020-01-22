e2ded3860ad6e16fed3dc4acf7e75e218d67cda5
Kureha <59548357+Mkureha@users.noreply.github.com>
2020-01-22 14:56:05 +0900

    Update MemberBean.java

--- Eclipse/MemberBean.java
+++ Eclipse/MemberBean.java
@@ -6,8 +6,15 @@ public class MemberBean
     // 프로퍼티에 직접 접근할 수 없게 private를 사용한다.
     private String id;
     private String pw;
-    private String addr;
+    private String Addr;
     private String tel;
+    private String Name;
+    private String Gender;
+    private String Mail1;
+    private String Mail2;
+    private String Birthyy;
+    private String Birthmm;
+    private String Birthdd;
     
     /* 데이터를 가져오거나(get), 세팅하는(set)
     *  기능을 하는 메서드를 만든다.
@@ -26,16 +33,59 @@ public class MemberBean
     public void setPw(String pw) {
         this.pw = pw;
     }
-    public String getTel() {
+    public String gettel() {
         return tel;
     }
     public void setTel(String tel) {
         this.tel= tel;
     }
     public String getAddr() {
-        return addr;
+        return Addr;
     }
-    public void setAddr(String addr) {
-        this.addr = addr;
-    }    
+    public void setAddr(String Addr) {
+        this.Addr = Addr;
+        
+    }
+    public void setName(String Name) {
+        this.id = Name;
+    }
+    public String getName() {
+        return Name;
+    }
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+    public String getGender() {
+        return Gender;
+    }
+    public void setMail1(String Mail1) {
+        this.Mail1= Mail1;
+    }
+    public String getMail1() {
+        return Mail1;
+    }
+    public void setMail2(String Mail2) {
+        this.Mail2 = Mail2;
+    }
+    public String getMail2() {
+        return Mail2;
+    }
+    public void setBirthyy(String Birthyy) {
+        this.Birthyy = Birthyy;
+    }
+    public String getBirthyy() {
+        return Birthyy;
+    }
+    public void setBirthmm(String Birthmm) {
+        this.Birthmm = Birthmm;
+    }
+    public String getBirthmm() {
+        return Birthmm;
+    }
+    public void setBirthdd(String Birthdd) {
+        this.Birthdd= Birthdd;
+    }
+    public String getBirthdd() {
+        return Birthdd;
+    }
 }
